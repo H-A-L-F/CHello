7167f52163d52a670721bc038756a2a398a55af3
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-06-16 12:28:45 +0700

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import './index.css';
+
 function App() {
-  return null;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;

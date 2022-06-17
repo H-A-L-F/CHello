6842bf3493e7d7c4350b9b3d264e9e939d8b36b2
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-06-17 11:41:32 +0700

    sidenavbar

--- src/views/components/sidebar/SideBar.js
+++ src/views/components/sidebar/SideBar.js
@@ -1,17 +1,23 @@
+import { HiCalendar, HiHome, HiOutlinePlusSm } from "react-icons/hi";
+
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-neutral text-neutral-content shadow-lg">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <SideBarIcon icon={<HiHome size = "28"/>} text="Home" />
+            <SideBarIcon icon={<HiOutlinePlusSm size = "32"/>} text="Create Workspace" />
+            <SideBarIcon icon={<HiCalendar size = "20"/>} text="Workspace"/>
         </div>
     );
 }
 
-const SideBarIcon = ({ icon }) => (
-    
+const SideBarIcon = ({ icon, text }) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
 );
 
 export default SideBar;

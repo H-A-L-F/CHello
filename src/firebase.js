dc10dbfa89dd16cfea605f2a253c2b98fdf99e6a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-06-17 13:32:47 +0700

    electron done

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA84MNVdAoXlVU1eHTqkz_HStw6pOGPlIc",
+  authDomain: "chello-31ee4.firebaseapp.com",
+  projectId: "chello-31ee4",
+  storageBucket: "chello-31ee4.appspot.com",
+  messagingSenderId: "347877435439",
+  appId: "1:347877435439:web:235d1cd3c399d1ca7cfeda"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

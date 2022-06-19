de3c563c8238b88c37fed8ad0b70660b92754c30
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-06-19 17:16:24 +0700

    custom modal

--- src/views/components/SideBar.js
+++ src/views/components/SideBar.js
@@ -5,15 +5,10 @@ const SideBar = () => {
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-neutral text-neutral-content shadow-lg">
             <SideBarIcon icon={<HiHome size = "28"/>} text="Home" />
 
-            <label for="my-modal-4" class="modal-button"><SideBarIcon icon={<HiOutlinePlusSm size = "32"/>} text="Create Workspace" /></label>
+            <SideBarIcon icon={<HiOutlinePlusSm size = "32"/>} text="Create Workspace" />
 
-            <input type="checkbox" id="my-modal-4" class="modal-toggle" />
-            <label for="my-modal-4" class="modal cursor-pointer">
-            <label class="modal-box relative" for="">
-                <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
-                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-            </label>
-            </label>
+            <Modal sidebar={<SideBarIcon icon={<HiOutlinePlusSm size = "32"/>} text="Create Workspace" />} target="modal-cws"/>
+            <ModalContent target="modal-cws"/>
 
             <SideBarIcon icon={<HiCalendar size = "20"/>} text="Workspace"/>
 
@@ -21,8 +16,8 @@ const SideBar = () => {
     );
 }
 
-const SideBarIcon = ({ icon, text, content }) => (
-    <div for="my-modal-4" className="sidebar-icon modal-button group">
+const SideBarIcon = ({ icon, text }) => (
+    <div className="sidebar-icon group">
         {icon}
 
         <span className="sidebar-tooltip group-hover:scale-100">
@@ -31,5 +26,25 @@ const SideBarIcon = ({ icon, text, content }) => (
 
     </div>
 );
+
+const Modal = ({ sidebar, target }) => {
+    return (
+        <label for={target} class="modal-button"> {sidebar} </label>
+    );
+}
+
+const ModalContent = ({ target }) => {
+    return (
+        <div>
+            <input type="checkbox" id={target} class="modal-toggle" />
+            <label for={target} class="modal cursor-pointer">
+            <label class="modal-box relative" for="">
+                <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            </label>
+            </label>
+        </div>
+    );
+}
 
 export default SideBar;

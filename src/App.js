3defc112245f1758420030350adceb4b416a8a17
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-06-21 09:55:06 +0700

    middleware

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate, useLocation } from "react-router-dom";
 import { UserAuthContextProvider, useUserAuth } from "./AuthContext";
 import "./index.css";
 import MainLayout from "./views/layout/MainLayout";
+import Login from "./views/pages/Login";
 import Register from "./views/pages/Register";
 
 function requireUser(nextState, replace, next) {
@@ -24,11 +25,31 @@ function App() {
       <UserAuthContextProvider>
         <Routes>
           <Route path="/register" element={<Register />} />
-          <Route path="/main/*" element={<MainLayout />} onEnter={requireUser}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/main/*" element={
+            <RequireAuth>
+              <MainLayout />
+            </RequireAuth>
+          }/>
         </Routes>
       </UserAuthContextProvider>
     </BrowserRouter>
   );
+}
+
+function RequireAuth({ children }) {
+  let auth = useUserAuth();
+  let location = useLocation();
+
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 }
 
 export default App;

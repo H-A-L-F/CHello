2d0e1ca405d1f76f6c065bc20e5d12e516191bcf
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-06-21 08:19:08 +0700

    middleware attempt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { UserAuthContextProvider } from './AuthContext';
-import './index.css';
-import MainLayout from './views/layout/MainLayout';
-import Register from './views/pages/Register';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserAuthContextProvider, useUserAuth } from "./AuthContext";
+import "./index.css";
+import MainLayout from "./views/layout/MainLayout";
+import Register from "./views/pages/Register";
+
+function requireUser(nextState, replace, next) {
+  if (() => {
+    const {user} = useUserAuth()
+    return user
+  }) {
+    replace({
+      pathname: "/register",
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
+  next();
+}
 
 function App() {
+
   return (
     <BrowserRouter>
       <UserAuthContextProvider>
         <Routes>
-          <Route path="/register" element={<Register />}/>
-          <Route path="/main/*" element={<MainLayout />}/>
+          <Route path="/register" element={<Register />} />
+          <Route path="/main/*" element={<MainLayout />} onEnter={requireUser}/>
         </Routes>
       </UserAuthContextProvider>
     </BrowserRouter>

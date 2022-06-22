6892bd9cd564811f0b52fc6bd8c465041787a3cb
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-06-23 06:16:41 +0700

    refactor member system

--- src/views/components/AdminForm.js
+++ src/views/components/AdminForm.js
@@ -2,13 +2,7 @@ import { onSnapshot } from "firebase/firestore"
 import { useEffect, useState } from "react"
 
 function AdminForm() {
-    const [members, setMembers] = useState()
 
-    const adminCollectio
-
-    useEffect(() => {
-        const unsub = onSnapshot()
-    }, [])
 
   return (
     <div>
